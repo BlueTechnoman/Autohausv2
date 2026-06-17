3179ee459076cb4d1089274e10984bc146ae5af0
BlueTechnoman <raphael.horlbeck@icloud.com>
2026-06-17 10:46:22 +0200

    appointment und documents und accounts

--- backend/appointments/models.py
+++ backend/appointments/models.py
@@ -2,18 +2,22 @@ from django.db import models
 
 # Create your models here.
 
+from django.db import models
 from customers.models import Customer
+from accounts.models import User
 
 class Appointment(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    staff = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     title = models.CharField(max_length=100)
     date = models.DateTimeField()
 
-    TYPE = [
+    TYPE_CHOICES = [
         ("testdrive", "Probefahrt"),
         ("consulting", "Beratung"),
         ("handover", "Übergabe"),
+        ("service", "Werkstatt"),
     ]
 
-    type = models.CharField(max_length=20, choices=TYPE)
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
--- backend/documents/models.py
+++ backend/documents/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+from django.db import models
 from customers.models import Customer
 from sales.models import Sale
 
@@ -9,5 +10,6 @@ class Document(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True, blank=True)
     sale = models.ForeignKey(Sale, on_delete=models.CASCADE, null=True, blank=True)
 
+    title = models.CharField(max_length=100)
     file = models.FileField(upload_to="documents/")
-    created_at = models.DateTimeField(auto_now_add=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
