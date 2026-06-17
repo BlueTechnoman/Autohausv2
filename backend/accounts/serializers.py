4833b89f816e8d5201be95f49ebf7bb63efe8817
BlueTechnoman <raphael.horlbeck@icloud.com>
2026-06-17 16:15:35 +0200

    asdasd

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -3,16 +3,23 @@ from rest_framework import serializers
 
 User = get_user_model()
 
+
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ["username", "email", "password"]
+        fields = [
+            "username",
+            "email",
+            "password",
+            "role",
+        ]
 
     def create(self, validated_data):
         return User.objects.create_user(
             username=validated_data["username"],
             email=validated_data.get("email"),
             password=validated_data["password"],
+            role=validated_data["role"],
         )
