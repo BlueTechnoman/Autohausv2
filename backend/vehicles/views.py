8e75eb6e4fa0f0818c16fa7e482065ba216a419c
BlueTechnoman <raphael.horlbeck@icloud.com>
2026-06-17 09:41:58 +0200

    chmod -r 777

--- backend/vehicles/views.py
+++ backend/vehicles/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework.viewsets import ModelViewSet
+from .models import Vehicle
+from .serializers import VehicleSerializer
+
+class VehicleViewSet(ModelViewSet):
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
