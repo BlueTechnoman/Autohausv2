aabc4c36d2235585d2b29150fe01d388c54ab2a7
BlueTechnoman <raphael.horlbeck@icloud.com>
2026-06-18 13:52:26 +0200

    asdas

--- backend/sales/models.py
+++ backend/sales/models.py
@@ -1,10 +1,7 @@
 from django.db import models
-
-# Create your models here.
-
-from django.db import models
 from customers.models import Customer
 from vehicles.models import Vehicle
+
 
 class Sale(models.Model):
     customer = models.ForeignKey(
@@ -18,15 +15,32 @@ class Sale(models.Model):
         on_delete=models.CASCADE
     )
 
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    date = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    date = models.DateTimeField(
+        auto_now_add=True
+    )
 
     STATUS = [
         ("offer", "Angebot"),
         ("sold", "Verkauft"),
     ]
 
-    status = models.CharField(max_length=20, choices=STATUS, default="offer")
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS,
+        default="offer"
+    )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.status == "sold":
+            self.vehicle.status = "sold"
+            self.vehicle.save()
 
     def __str__(self):
         return f"{self.customer} - {self.vehicle}"
